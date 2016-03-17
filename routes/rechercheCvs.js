a2dc852552219ab48fdda776f4ab899f3ffb111d
BendadaAchraf <bendada.achraf@gmail.com>
2016-03-17 03:54:36 +0000

    RechercheCV 2
    ajout de recherche  par competence

--- routes/rechercheCvs.js
+++ routes/rechercheCvs.js
@@ -1,13 +1,19 @@
 /**
  * Created by Achraf on 26/02/2016.
  */
-var models  = require('../models');
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('rechercheCvs', { title: 'Recherche d\'Cvs', messageErr:null });
+router.get('/', function (req, res, next) {
+    models.Niveau_etude.findAll({
+        attributes: ['id', 'intitule']
+    }).then(function (niveau) {
+        niveauEtude = niveau;
+
+        res.render('rechercheCvs', {title: 'Recherche d\'Cvs', messageErr: null});
+    });
 });
 
 
@@ -15,30 +21,44 @@ router.post('/', function (req, res) {
     console.log(req.body);
 
     //Critère pour le niveau d'etude
-    if(req.body.nivEtude){
+    if (req.body.nivEtude) {
         var niv_etude = {intitule: req.body.nivEtude};
-    }else{
+    } else {
         var niv_etude = null;
     }
+    if (req.body.competence) {
+        var competences = req.body.competence;
+    } else {
+        var competence = null;
+    }
+
 
     //Vérification s'il y a au moins un critère selectionné
-    if( niv_etude == null){
+    if (niv_etude == null) {
         //Envoie d'un message d'avertissement : Veuillez selectionner au mois un critère
-        res.render('rechercheCvs', { title: 'Recherche d\'Cvs', messageErr:"Veuillez selectionner au mois un critère" });
-    }else {
+        res.render('rechercheCvs', {title: 'Recherche d\'Cvs', messageErr: "Veuillez selectionner au mois un critère"});
+    } else {
 
         var criteres_cv = {};
 
+
         //Construction de la requete de recherche
-        criteres_cv.include = [{model: models.Niveau_etude, where: niv_etude},];
+
+
+        criteres_cv.include = [{model: models.Niveau_etude, where: niv_etude}];
+        if (competences != null) {
+            console.log(competences);
+            criteres_cv.include.push({model: models.Competence_CV, where: competences});
+        }
+
 
         //Execution de la requete de type SELECT
-        models.CV.findAll(criteres_cv).then(function(Cvs){
+        models.CV.findAll(criteres_cv).then(function (Cvs) {
             //Liste des mois
-            var mois = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
+            var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
             //Affichage de la page de resultats
-            res.render('resultatRechercheCvs', {title: 'Résultat de recherche d\'Cvs', CV: Cvs, mois: mois });
+            res.render('resultatRechercheCvs', {title: 'Résultat de recherche d\'Cvs', CV: Cvs, mois: mois});
             console.log(JSON.stringify(Cvs));
         });
 
